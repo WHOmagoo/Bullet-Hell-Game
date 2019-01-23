38686418ec8f17420c8e411e839c7e7d07b3bedc
hmcgough <hrmagoo@gmail.com>
2019-01-22 21:04:09 -0800

    Refactor classes into respective sub folders

--- BulletHell/Path.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BulletHell
-{
-    public abstract class Path
-    {
-        public abstract Tuple<double, double> updateLocation(Tuple<double, double> curLocation, int currentTick);
-    }
-}
