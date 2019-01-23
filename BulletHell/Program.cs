6ee0868f431f6e95bd3b90c2f608ae25fb80d655
hmcgough <hrmagoo@gmail.com>
2019-01-22 21:00:24 -0800

    Simple main function, doesn't really do anything right now.

--- BulletHell/Program.cs
+++ BulletHell/Program.cs
@@ -7,6 +7,11 @@ namespace BulletHell
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            LinearPath path = new LinearPath(0,4);
+
+            Tuple<double, double> result = path.updateLocation(5);
+
+            Console.WriteLine(result);
         }
     }
 }
