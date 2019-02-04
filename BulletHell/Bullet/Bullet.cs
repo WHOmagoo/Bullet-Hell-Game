831ce0bd37981308452ca38245a24ef898947c74
whomagoo <ros_MC_hrmcgough>
2019-02-03 21:33:40 -0800

    Change Bullet to reflect proper use of Path

--- BulletHell/Bullet/Bullet.cs
+++ BulletHell/Bullet/Bullet.cs
@@ -1,43 +1,28 @@
 using System;
+using Microsoft.Xna.Framework;
 
 namespace BulletHell
 {
     public abstract class Bullet
     {
         private int lastUpdate;
-        private Tuple<double, double> currentLocation;
-        private Tuple<double, double> offset;
-        private int startingTick;
+        private Vector2 currentLocation;
 
-        private Path path;
+        private Path _path;
 
-        public Bullet(Path path, int locX, int locY, int startingTick)
+        public Bullet(Path path, int locX, int locY)
         {
-            this.path = path;
-            
-            //Save the global starting tick that this object was created on
-            this.startingTick = startingTick;
+            this._path = path;
             
             //Save the current location of the bullet
-            currentLocation = new Tuple<double, double>(locX, locY);
-            
-            //tmp variable of the initial position based upon the path
-            Tuple<double, double> tmp = path.updateLocation(0);
-            
-            //Calculate the x and y translations of the bullet when compared to thee path
-            offset = new Tuple<double, double>(tmp.Item1 - locX, tmp.Item2 - locY);
+            currentLocation = new Vector2(locX, locY);
         }
 
         public abstract double calculateDamage();
 
-        public void updateLocation(int newTime)
+        public void updateLocation()
         {
-            //Find the new location the bullet should be in. We pass in the amount of ticks elapsed since the object started moving
-            Tuple<double, double> tmp = path.updateLocation(newTime - startingTick);
-            
-            //Account for the initial calculated translation in final output of update location
-            currentLocation = new Tuple<double, double>(tmp.Item1 + offset.Item1, tmp.Item2 + offset.Item2);
-            
+            currentLocation = _path.UpdateLocation();
             //TODO implement the notify pattern to alert subscribed objects that our location has changed
         }
     }
