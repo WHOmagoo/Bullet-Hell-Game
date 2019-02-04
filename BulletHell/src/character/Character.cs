20153df8f61f2182ab23c3456215a5dc0d74b7d5
whomagoo <ros_MC_hrmcgough>
2019-02-04 12:00:58 -0800

    Merge Hannah into unstable

---
+++ BulletHell/src/character/Character.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace test4
+{
+    public class Character : Entity
+    {
+        private int healthPoints;
+        private bool hitBox;    // bool representing whether or not to display hitbox
+        //protected Gun gunEquipped;  //need Gun class
+
+        public Character(Canvas canvas, Texture2D texture, Vector2 startLocation) : base(canvas,texture,startLocation)
+        {
+            hitBox = false;
+            healthPoints = 1000;    // just chose a random value of 1000 for now (value may depend on which character)
+        }
+
+        public Character(Canvas canvas, Texture2D texture, Rectangle rect) : base(canvas, texture, rect)
+        {
+            hitBox = false;
+            healthPoints = 1000;    // (same as above)
+        }
+
+        /*public void Shoot()
+        {
+            //need gun class   TODO
+            gun.Shoot();
+        }*/
+
+        public bool ShowHitbox
+        {
+            get { return hitBox; }
+            set { hitBox = value; }
+
+        }
+
+        /*public OnHit(Bullet bullet)
+        {
+            //need bullet class     TODO
+        }*/
+
+
+    }
+}
