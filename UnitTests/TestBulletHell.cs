4dc35ebd5df7bcbd3b54d89e4f7ab7810a0c764b
hmcgough <hrmagoo@gmail.com>
2019-02-05 21:25:40 -0800

    Refactor UnitTests to match definition in BulletHell

--- UnitTests/TestBulletHell.cs
+++ UnitTests/TestBulletHell.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.ConstrainedExecution;
-using BulletHell.Engine;
+using BulletHell.GameEngine;
+using Microsoft.Xna.Framework;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -12,18 +13,18 @@ namespace UnitTests
         public void TestLinearPathMovementInXDirection()
         {
             Assert.False(false);
-            LinearPath path = new LinearPath(0, 1);
-
-            Tuple<double, double> result = path.updateLocation(1);
+            LinearLocationEquation path = new LinearLocationEquation(0, 1);
+            
+            Vector2 result = path.GetLocation(1);
 
             double allowance = .01;
-            Assert.AreEqual(1, result.Item1, allowance);
-            Assert.AreEqual(0, result.Item2, allowance);
+            Assert.AreEqual(1, result.X, allowance);
+            Assert.AreEqual(0, result.Y, allowance);
 
-            result = path.updateLocation(9);
+            result = path.GetLocation(9);
             
-            Assert.AreEqual(9, result.Item1, allowance);
-            Assert.AreEqual(0, result.Item2, allowance);
+            Assert.AreEqual(9, result.X, allowance);
+            Assert.AreEqual(0, result.Y, allowance);
         }
         
         [Test]
@@ -32,22 +33,22 @@ namespace UnitTests
             double allowance = .01;
             
             Assert.True(true);
-            LinearPath path = new LinearPath(Math.PI / 2, 1);
+            LinearLocationEquation path = new LinearLocationEquation(Math.PI / 2, 1);
 
-            Tuple<double, double> result = path.updateLocation(1);
+            Vector2 result = path.GetLocation(1);
             
-            Assert.AreEqual(0, result.Item1, allowance);
-            Assert.AreEqual(1, result.Item2, allowance);
+            Assert.AreEqual(0, result.X, allowance);
+            Assert.AreEqual(1, result.Y, allowance);
 
-            result = path.updateLocation(9);
+            result = path.GetLocation(9);
 
-            Assert.AreEqual(0, result.Item1, allowance);
-            Assert.AreEqual(9, result.Item2, allowance);
+            Assert.AreEqual(0, result.X, allowance);
+            Assert.AreEqual(9, result.Y, allowance);
 
-            result = path.updateLocation(5000);
+            result = path.GetLocation(5000);
             
-            Assert.AreEqual(0, result.Item1, allowance);
-            Assert.AreEqual(5000, result.Item2, allowance);
+            Assert.AreEqual(0, result.X, allowance);
+            Assert.AreEqual(5000, result.Y, allowance);
         }
     }
 }
