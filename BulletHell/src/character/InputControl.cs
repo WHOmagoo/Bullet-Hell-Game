7cc68464d04dfff23553e2a301b55881dc1e83f1
whomagoo <ros_MC_hrmcgough>
2019-02-06 18:45:47 -0800

    Remove unnecessary imports

--- BulletHell/src/character/InputControl.cs
+++ BulletHell/src/character/InputControl.cs
@@ -1,15 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-
 namespace BulletHell.GameEngine
 {
     class InputControl
