08e87e08c22b7106084c75564394f619207c49d9
whomagoo <ros_MC_hrmcgough>
2019-02-26 01:20:54 -0800

    Remove Gun from Player class and use gun from parent class

--- BulletHell/src/character/Player.cs
+++ BulletHell/src/character/Player.cs
@@ -15,8 +15,6 @@ namespace BulletHell.GameEngine
         internal Vector2 direction = Vector2.Zero;
         internal Vector2 speed = Vector2.Zero;
 
-        private Gun gun;
-
         Canvas canvas;
         int slow = 0;
 
@@ -24,7 +22,7 @@ namespace BulletHell.GameEngine
         {
             this.canvas = canvas;
             InputControl.AssignPlayer(this);
-            gun = new BasicGun(1, new LinearLocationEquation(-Math.PI / 2, 1), GraphicsLoader.getGraphicsLoader().getBulletTexture(), 1000, true);
+            gunEquipped = new BasicGun(1, new LinearLocationEquation(-Math.PI / 2, 1), GraphicsLoader.getGraphicsLoader().getBulletTexture(), 1000, true);
             
         }
 
@@ -91,7 +89,7 @@ namespace BulletHell.GameEngine
 
             if (currState.IsKeyDown(Keys.Space))
             {
-                gun.shoot(Location);
+                gunEquipped.Shoot(Location);
             }
         }
     }
