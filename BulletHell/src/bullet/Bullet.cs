fef60c5fc8ff881d2f595e9c8bfc47aaaa296b86
hmcgough <hrmagoo@gmail.com>
2019-02-28 22:34:16 -0800

    Remove Canvas from Bullet

--- BulletHell/src/bullet/Bullet.cs
+++ BulletHell/src/bullet/Bullet.cs
@@ -9,7 +9,7 @@ namespace BulletHell.GameEngine
         Path pathToFollow;
 
         //TODO decide if we should take in ILocationEquation and make a path or accept a Path object within bullet
-        public Bullet(int damage, ILocationEquation locationEquation, Canvas canvas, Texture2D texture, Vector2 startLocation) : base( texture, startLocation)
+        public Bullet(int damage, ILocationEquation locationEquation, Texture2D texture, Vector2 startLocation) : base( texture, startLocation)
         {
             this.damage = damage;
             this.pathToFollow = new Path(locationEquation, startLocation, 0);
