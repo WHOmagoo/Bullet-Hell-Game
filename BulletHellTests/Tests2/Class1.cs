92b8fcb66775b2c5905b6826109dde4f6cee85c4
sinouye <sean.inouye@wsu.edu>
2019-03-03 20:06:00 -0800

    added new test file

---
+++ BulletHellTests/Tests2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BulletHellTests.GameEngine;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestLinearPathMovementInXDirection()
+        {
+            Assert.False(false);
+            LinearLocationEquation path = new LinearLocationEquation(0, 1);
+
+            Vector2 result = path.GetLocation(1);
+
+            double allowance = .01;
+            Assert.AreEqual(1, result.X, allowance);
+            Assert.AreEqual(0, result.Y, allowance);
+
+            result = path.GetLocation(9);
+
+            Assert.AreEqual(9, result.X, allowance);
+            Assert.AreEqual(0, result.Y, allowance);
+        }
+    }
+}
