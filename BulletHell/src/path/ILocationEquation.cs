f9493f27eeb4f5ee447f00dedbfb60bfefc7ca29
sinouye <sean.inouye@wsu.edu>
2019-03-03 20:13:41 -0800

    updated .gitignore. Only test folder now shown

--- BulletHell/src/path/ILocationEquation.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework;
-
-namespace BulletHellTests.GameEngine
-{
-    public interface ILocationEquation
-    {
-        Vector2 GetLocation(long ticksElapsed);
-    }
-}
--- BulletHellTests/src/Globals.cs
+++
@@ -1,6 +0,0 @@
-namespace BulletHellTests.GameEngine 
-{
-    public static class Globals {
-        // static int MAX_ENTITIES_ON_CANVAS = 5000;
-    }
-}
