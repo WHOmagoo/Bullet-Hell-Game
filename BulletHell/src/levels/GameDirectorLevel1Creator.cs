b4956bf8028e7a576eb6b8fd9670912639640370
Christian Burke <ChristianSBurke@Gmail.com>
2019-03-09 20:42:10 -0800

    improvements to gameplay

--- BulletHell/src/levels/GameDirectorLevel1Creator.cs
+++ BulletHell/src/levels/GameDirectorLevel1Creator.cs
@@ -17,39 +17,32 @@ namespace BulletHell.levels
         private Canvas canvas;
         private GameDirector director;
         private CollisionManager collisionManager;
+
+        
+        private enum MOVEMENT { DOWN_RIGHT, DOWN_LEFT}
         public Tuple<GameDirector, Canvas, CollisionManager> makeGame(GraphicsDevice graphicsDevice)
         {
             director = new GameDirector();
             canvas = new Canvas(new SpriteBatch(graphicsDevice));
             collisionManager = new CollisionManager();
 
-            playerTexture = Texture2D.FromStream(graphicsDevice,
-                new FileStream("Content/sprites/shuttle.png", FileMode.Open));
+            int SCREEN_WIDTH = graphicsDevice.Viewport.Bounds.Width;
+            int SCREEN_HEIGHT = graphicsDevice.Viewport.Bounds.Height;
 
-            enemyATexture = Texture2D.FromStream(graphicsDevice,
-                new FileStream("Content/sprites/enemyA.png", FileMode.Open));
+            LoadTextures(graphicsDevice);
 
-            enemyBTexture = Texture2D.FromStream(graphicsDevice,
-                new FileStream("Content/sprites/enemyB.png", FileMode.Open));
-
-            Player player = new Player(canvas, playerTexture, new Vector2(graphicsDevice.Viewport.Bounds.Width / 2 - playerTexture.Width / 2, 300));
+            Player player = new Player(canvas, playerTexture, new Vector2(SCREEN_WIDTH / 2 - playerTexture.Width / 2, 300));
             player.SetSize(72, 100);
             player.PropertyChanged += canvas.OnWeaponChange;
             player.gunEquipped.GunShotHandler += canvas.OnGunShot;
             player.Hitbox = new CollidingCircle(player.Location, new Vector2(player.Rect.Width / 2, player.Rect.Height / 2), 15);
             collisionManager.addToTeam(player, TEAM.FRIENDLY);
 
-            Vector2 topMiddle = new Vector2(graphicsDevice.Viewport.Bounds.Width / 2, -100);
+            Vector2 topMiddle = new Vector2(SCREEN_WIDTH / 2, -100);
 
-            // Enemy e1 = MakeEnemy('a', 0, new Vector2(500,0));
-            Enemy e1 = MakeEnemy('a', 0, topMiddle);
-            Enemy e2 = MakeEnemy('a', 0, new Vector2(0,0));
-
-            // Enemy enemy2 = new EnemyB(enemyBTexture, new Vector2(graphicsDevice.Viewport.Bounds.Width / 2 - 50, -100));
-            // enemy2.SetSize(100, 100);
-            // enemy2.PropertyChanged += canvas.OnWeaponChange;
-            // enemy2.gunEquipped.GunShotHandler += canvas.OnGunShot;
-            // collisionManager.addToTeam(enemy1, TEAM.ENEMY);
+            Enemy e1 = MakeEnemy('a', MOVEMENT.DOWN_RIGHT, topMiddle);
+            Enemy e2 = MakeEnemy('a', MOVEMENT.DOWN_RIGHT, new Vector2(SCREEN_WIDTH / 4,-100));
+            Enemy e3 = MakeEnemy('a', MOVEMENT.DOWN_LEFT, new Vector2(3 * SCREEN_WIDTH / 4,-100));
 
             Texture2D midBossTexture = Texture2D.FromStream(graphicsDevice,
                 new FileStream("Content/sprites/midboss.png", FileMode.Open));
@@ -72,16 +65,17 @@ namespace BulletHell.levels
             // collisionManager.addToTeam(finalboss, TEAM.ENEMY);
 
 
-            director.addEvent(0, new CreateEnemyEvent(collisionManager, canvas, e1));
             director.addEvent(0, new PlayerEnter(canvas, player));
-            director.addEvent(15 * 10000, new CreateEnemyEvent(collisionManager, canvas, e2));
+            director.addEvent(0, new CreateEnemyEvent(collisionManager, canvas, e1));
+            director.addEvent(5 * 10000, new CreateEnemyEvent(collisionManager, canvas, e2));
+            director.addEvent(5 * 10000, new CreateEnemyEvent(collisionManager, canvas, e3));
             director.addEvent(44 * 10000, new CreateEnemyEvent(collisionManager, canvas, midboss));
             director.addEvent(80 * 10000, new CreateEnemyEvent(collisionManager, canvas, finalboss));
 
             return new Tuple<GameDirector, Canvas, CollisionManager>(director, canvas, collisionManager);
         }
 
-        private Enemy MakeEnemy(char enemyType, int movementType, Vector2 startLocation)
+        private Enemy MakeEnemy(char enemyType, MOVEMENT movementType, Vector2 startLocation)
         {
             Enemy e;
             if (enemyType == 'a')
@@ -101,30 +95,51 @@ namespace BulletHell.levels
             e.PropertyChanged += canvas.OnWeaponChange;
             e.gunEquipped.GunShotHandler += canvas.OnGunShot;
             
-            GameEngine.Path p = MakePath(0, startLocation);
+            GameEngine.Path p = MakePath(movementType, startLocation);
             e.SetPath(p);
             return e;
         }
 
-        private GameEngine.Path MakePath(int movementType, Vector2 location)
+        private GameEngine.Path MakePath(MOVEMENT movementType, Vector2 location)
         {
-            if (movementType == 0)
+            if (movementType == MOVEMENT.DOWN_RIGHT)
             {
-                ILocationEquation down = new LinearLocationEquation(Math.PI / 2, .04F);
+                ILocationEquation down = new LinearLocationEquation(Math.PI / 2, .06F);
                 ILocationEquation stayStill = StayStill.getStayStill();
-                ILocationEquation right = new LinearLocationEquation(0, .04F);
+                ILocationEquation right = new LinearLocationEquation(0, .08F);
                 List<Tuple<ILocationEquation, long>> piecewiseLocationEquations = new List<Tuple<ILocationEquation, long>>();
-                piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(down, 1000 * 5));
+                piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(down, 1000 * 3));
                 piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(stayStill, 1000 * 7));
                 piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(right, 1000 * 100));
                 PiecewiseLocationEquation locationEquation = new PiecewiseLocationEquation(piecewiseLocationEquations);
                 return new GameEngine.Path(locationEquation, location, 0);
             }
-            else if (movementType == 1)
+            else if (movementType == MOVEMENT.DOWN_LEFT)
             {
+                ILocationEquation down = new LinearLocationEquation(Math.PI / 2, .06F);
+                ILocationEquation stayStill = StayStill.getStayStill();
+                ILocationEquation left = new LinearLocationEquation(Math.PI, .08F);
+                List<Tuple<ILocationEquation, long>> piecewiseLocationEquations = new List<Tuple<ILocationEquation, long>>();
+                piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(down, 1000 * 3));
+                piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(stayStill, 1000 * 7));
+                piecewiseLocationEquations.Add(new Tuple<ILocationEquation, long>(left, 1000 * 100));
+                PiecewiseLocationEquation locationEquation = new PiecewiseLocationEquation(piecewiseLocationEquations);
+                return new GameEngine.Path(locationEquation, location, 0);
 
             }
             throw new NotImplementedException();
+        }
+
+        private void LoadTextures(GraphicsDevice graphicsDevice)
+        {
+            playerTexture = Texture2D.FromStream(graphicsDevice,
+                new FileStream("Content/sprites/shuttle.png", FileMode.Open));
+
+            enemyATexture = Texture2D.FromStream(graphicsDevice,
+                new FileStream("Content/sprites/enemyA.png", FileMode.Open));
+
+            enemyBTexture = Texture2D.FromStream(graphicsDevice,
+                new FileStream("Content/sprites/enemyB.png", FileMode.Open));
         }
     }
 
