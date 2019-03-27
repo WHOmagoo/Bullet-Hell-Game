33127b69023e55d9909e9d61ba7c1405f66fb20d
hmcgough <hrmagoo@gmail.com>
2019-03-27 01:19:54 -0700

    Modify the way that Player interacts with the Controller class
    It now requires a Controller object and will subscribe to events given by it.

--- BulletHell/src/character/Player.cs
+++ BulletHell/src/character/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using BulletHell.controls;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
@@ -25,7 +26,7 @@ namespace BulletHell.GameEngine
 
         public event EventHandler DeathEvent;
 
-        public Player(Canvas canvas, Texture2D texture, Vector2 startLocation) : base(texture, startLocation)
+        public Player(Canvas canvas, Texture2D texture, Vector2 startLocation, Controller controller) : base(texture, startLocation)
         {
             this.respawnLocation = startLocation;
             this.canvas = canvas;
@@ -37,6 +38,18 @@ namespace BulletHell.GameEngine
 
             //TODO make this be a parameter
             healthPoints = 10;      //player lives
+            subscribeToController(controller);
+        }
+
+        private void subscribeToController(Controller controller)
+        {
+            controller.OnLeft += goLeft;
+            controller.OnRight += goRight;
+            controller.OnUp += goUp;
+            controller.OnShoot += Shoot;
+            controller.OnSlow += goSlow;
+            controller.OnFast += goFast;
+            controller.OnDown += goDown;
         }
 
         public void LoadContent(ContentManager theContentManager)
@@ -54,8 +67,7 @@ namespace BulletHell.GameEngine
             base.Update();
             float timeE = Clock.getClock().getTimeSinceLastUpdate();
             float scale = 0.5F;
-            KeyboardState currState = Keyboard.GetState();
-            UpdateMove();
+            
             if (slow == 1) timeE *= scale;
 
             if (Rect.X + Rect.Width > canvas.GetBounds().Width)
@@ -69,30 +81,11 @@ namespace BulletHell.GameEngine
             {
                 Location += direction * speed * timeE / 50000;
             }
-            base.Update();
-        }
-
-        
-        private void UpdateMove()
-        {
-            speed = Vector2.Zero;
-            direction = Vector2.Zero;
-
-            Controller controller = new Controller();
-            controller.OnLeft += goLeft;
-            controller.OnRight += goRight;
-            controller.OnUp += goUp;
-            controller.OnDown += goDown;
-            controller.OnUpLeft += goUpLeft;
-            controller.OnUpRight += goUpRight;
-            controller.OnDownLeft += goDownLeft;
-            controller.OnDownRight += goDownRight;
-            controller.OnShoot += Shoot;
-            controller.OnSlow += goSlow;
-            controller.OnFast += goFast;
-
-            controller.Update();
             
+            direction = Vector2.Zero;
+            speed = Vector2.Zero;
+
+            base.Update();
         }
 
         private void goLeft(object sender, EventArgs e)
@@ -113,26 +106,6 @@ namespace BulletHell.GameEngine
         private void goDown(object sender, EventArgs e)
         {
             InputControl.MoveDown();
-        }
-
-        private void goUpLeft(object sender, EventArgs e)
-        {
-            // InputControl.MoveUpLeft();
-        }
-
-        private void goUpRight(object sender, EventArgs e)
-        {
-            // InputControl.MoveUpRight();
-        }
-
-        private void goDownLeft(object sender, EventArgs e)
-        {
-            // InputControl.MoveDownLeft();
-        }
-
-        private void goDownRight(object sender, EventArgs e)
-        {
-            // InputControl.MoveDownRight();
         }
 
         private void Shoot(object sender, EventArgs e)
