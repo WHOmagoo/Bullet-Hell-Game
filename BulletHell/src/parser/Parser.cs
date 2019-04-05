22439b7ff1da2a3dd4e9d3b4ef1bfc638ad074d6
james <james.a.nelson@wsu.edu>
2019-04-05 02:32:29 -0700

    impliment stub for parsers and interface for adding new parsers

---
+++ BulletHell/src/parser/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BulletHell
+{
+    public interface Parser
+    {
+        Dictionary<string, GameEngine.Enemy> getEnemyPrefabs();
+        Wave[] getWaves();
+        void Parse();
+
+    }
+}
---
+++ BulletHell/src/parser/XMLParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace BulletHell
+{
+    public class XMLParser : Parser
+    {
+        Dictionary<string, GameEngine.Enemy> enemyPrefabs;
+        Wave[] waves;
+        XElement level;
+
+
+        public XMLParser(string filename)
+        {
+            level = XElement.Load(filename);
+        }
+
+        public Dictionary<string, GameEngine.Enemy> getEnemyPrefabs() {
+            return enemyPrefabs;
+        }
+
+        public Wave[] getWaves() {
+            return waves;
+        }
+
+        public void Parse(){
+            foreach (var enemy in level.Elements("enemies")){
+                //TODO: impliment XML to dictionary mapping
+                // of prefabs
+            }
+
+            foreach (var wave in level.Elements("waves")) {
+                //TODO: impliment wave creation for each element
+                // and pushing it to the waves list
+            }
+        }
+    }
+}
