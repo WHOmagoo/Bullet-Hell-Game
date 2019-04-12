a4cf876ee69eda976099e28b227680efd60178f2
hmcgough <hrmagoo@gmail.com>
2019-04-12 05:28:55 -0700

    Add support for shotgun, basic, and surround guns in the make function.
    The choice can be made by passing a string to the function.

--- BulletHell/src/bullet/factory/BulletFactoryFactory.cs
+++ BulletHell/src/bullet/factory/BulletFactoryFactory.cs
@@ -1,43 +1,62 @@
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using BulletHell.gameEngine;
-using BulletHell.graphics;
 using BulletHell.path;
 
 namespace BulletHell.bullet.factory
 {
     public class BulletFactoryFactory
     {
+        
+        /// <summary>
+        /// the string key should be lowercase because the make function casts the name to lowercase when checking
+        /// </summary>
         private static Dictionary<string, Func<BulletFactory>> nameToFunction;
         
-        public static BulletFactory make(int fireRate = 1)
+        public static BulletFactory make(string gunName)
         {
             if(ReferenceEquals(null, nameToFunction))
             {
                 initializeDictionary();
             }
+
+            gunName = gunName.ToLower();
             
-            return new ShotgunBulletFactory(1, new LinearLocationEquation(6, 1));
+            if (nameToFunction.ContainsKey(gunName))
+            {
+                return nameToFunction[gunName]();
+            }
+            else
+            {
+                throw new Exception("Gun name not found");
+            }            
         }
 
+        
+        //TODO possible allow dynamically populating the nameToFunction Dictionary to allow loading of custom preset guns
         private static void initializeDictionary()
         {
             nameToFunction = new Dictionary<string, Func<BulletFactory>>();
             nameToFunction.Add("shotgun", makeDefaultShotgun);
             nameToFunction.Add("basic", makeBasicGun);
             nameToFunction.Add("surround", makeSurroundBulletFactory);
+            nameToFunction.Add("singlesinusoidal", makeSinusoidalBulletFactory);
             
+        }
+
+        private static BulletFactory makeSinusoidalBulletFactory()
+        {
+            return new SingleBulletFactory(new SinusoidalLocationEquation(90, 110, 200)); 
         }
 
         private static BulletFactory makeSurroundBulletFactory()
         {
-            return new SurroundBulletFactory(16, (float) Math.PI / 2, (float) (Math.PI / 9), new  SingleBulletFactory(new SinusoidalLocationEquation(90, 110, 200)));
+            return new SurroundBulletFactory(16, (float) Math.PI / 2, (float) (Math.PI / 9),
+                new  SingleBulletFactory(new SinusoidalLocationEquation(90, 110, 200)));
         }
 
         private static BulletFactory makeBasicGun()
         {
-            return new SingleBulletFactory(new LinearLocationEquation(0, .08F));
+            return new SingleBulletFactory(new LinearLocationEquation(0, .2F));
         }
 
         public static ShotgunBulletFactory makeDefaultShotgun()
