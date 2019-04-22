108b1d95327632d2f7ba7bc7e1e3bb47bfac302d
sinouye <sean.inouye@wsu.edu>
2019-04-22 06:23:22 -0700

    readded boss class

---
+++ BulletHell/src/gun/BossGun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using BulletHell.Graphics;
+
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace BulletHell.GameEngine
+{
+    public class BossGun : Gun
+    {
+
+        private float weaponSpread;
+        private float direction;
+        private int amountOfBullets;
+
+        public BossGun(int amountOfBullets, float direction, float spread, int damage, ILocationEquation shape, Texture2D texture, int delay, TEAM team) : base(damage, shape, texture, delay, team)
+        {
+            this.weaponSpread = spread;
+            this.direction = direction;
+            this.amountOfBullets = amountOfBullets;
+        }
+
+        public override void Shoot(Vector2 location)
+        {
+            if (canShoot())
+            {
+                var bullets = makeBullets(location);
+                base.wasShot();
+                OnShoot(bullets);
+            }
+        }
+
+        private List<Bullet> makeBullets(Vector2 location)
+        {
+            List<Bullet> result = new List<Bullet>();
+
+            Bullet b;
+            double increment = Math.PI / (amountOfBullets / 2d);
+            for (double i = 0; i < 2 * Math.PI; i += increment)
+            {
+                Path p = new BasicPath(fireShape, location, i);
+                b = new Bullet(damage, p, bulletTexture, team);
+                b.SetSize(20, 30);
+                b.Hitbox = new CollidingRectangle(b.Location, new Vector2(0, 0), 20, 30);
+                BHGame.CollisionManager.addToTeam(b, team);
+                result.Add(b);
+            }
+
+            return result;
+        }
+    }
+
+}
