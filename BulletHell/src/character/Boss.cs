b11c63dd360585e5f09a8519205660168c0ba7f7
Christian Burke <ChristianSBurke@Gmail.com>
2019-04-23 22:12:09 -0700

    demo boss

--- BulletHell/src/character/Boss.cs
+++ BulletHell/src/character/Boss.cs
@@ -21,18 +21,7 @@ namespace BulletHell.character
             base(texture, p ,health, bulletFactory, delay){ }
 
         public Boss(Enemy e, Vector2 startLocation) : base(e, startLocation)
-        {
-            // SpiralLocationEquation s = new SpiralLocationEquation(6, 40, 10);
-            // // BossGun g = new BossGun(Location+new Vector2(60,60), -2, 
-            // //     new SingleBulletFactory(new LinearLocationEquation(Math.PI/2, .2F)), 
-            // //     (float)(Math.PI/2), s, 
-            // //     GraphicsLoader.getGraphicsLoader().getTexture("enemy-bullet"), TEAM.ENEMY);
-            // // BulletFactory baseBf = new SingleBulletFactory(new LinearLocationEquation(Math.PI/2, .2F));
-            // BulletFactory baseBf = new ShotgunBulletFactory(2*Math.PI/3, new LinearLocationEquation(Math.PI/2, .2F));
-            // MovingBulletFactory bf = new MovingBulletFactory(baseBf, s);
-            // Gun g = new Gun(.01f, GraphicsLoader.getGraphicsLoader().getTexture("enemy-bullet"), bf, TEAM.ENEMY, 0);
-            // gunEquipped = g;
-        }
+        { }
 
         protected override void Die()
         {
